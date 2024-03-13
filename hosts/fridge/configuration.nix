12c693c4c8c9f8c7be3f8a6e862fe4d7f7d901aa
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-03-13 16:06:44 +0000

    Added auto-login to fridge host

--- hosts/fridge/configuration.nix
+++ hosts/fridge/configuration.nix
@@ -57,6 +57,10 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+	# Auto Login
+	services.xserver.displayManager.job.preStart = "sleep 5";
+	services.xserver.displayManager.autoLogin.enable = true;
+	services.xserver.displayManager.autoLogin.user = "francisco";
 
   # Configure keymap in X11
   services.xserver = {
