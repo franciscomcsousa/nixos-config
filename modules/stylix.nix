275dcb94b9cfd3dc45625ed96ba8f33388bca19d
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-06-26 21:10:49 +0100

    Add bibata cursor

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,26 +1,30 @@
 {pkgs, ...}: {
+
+  stylix = {
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    image = ./wallpaper.png;
+
+    fonts = {
+      monospace = {
+        package = (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];});
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+    };
   
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-  stylix.image = ./wallpaper.png;
-
-  stylix.fonts = {
-    monospace = {
-      package = (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];});
-      name = "JetBrainsMono Nerd Font Mono";
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
     };
-
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-
   };
-
-  stylix.targets.gtk.enable = false;
-
 }
