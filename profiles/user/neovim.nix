dc01c6167da793870904c84a9762a05315c87966
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-06-27 16:00:14 +0100

    Add neovim plugin git-signs

--- profiles/user/neovim.nix
+++ profiles/user/neovim.nix
@@ -22,14 +22,15 @@
         lualine-nvim
         vim-tmux-navigator
         nvim-treesitter.withAllGrammars
-        
+        gitsigns-nvim        
       ];
 
       extraLuaConfig = ''
         ${builtins.readFile "${configDir}/neovim/options.lua"}
 
         ${builtins.readFile "${configDir}/neovim/plugin/comment.lua"}
-        ${builtins.readFile "${configDir}/neovim/plugin/lualine.lua"}
+				${builtins.readFile "${configDir}/neovim/plugin/lualine.lua"}
+        ${builtins.readFile "${configDir}/neovim/plugin/git-signs.lua"}
         ${builtins.readFile "${configDir}/neovim/plugin/navigator.lua"}
         ${builtins.readFile "${configDir}/neovim/plugin/telescope.lua"}
         ${builtins.readFile "${configDir}/neovim/plugin/treesitter.lua"}
