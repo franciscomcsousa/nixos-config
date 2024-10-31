52a232d7a6bf8ce6e652282459d729a359b7d220
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-10-31 16:16:05 +0000

    Create lightbulb host (homeserver)

---
+++ hosts/lightbulb/configuration.nix
@@ -0,0 +1,82 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, inputs, profiles, ... }:
+
+{
+  imports = with profiles.environment;
+    [ # Include the results of the hardware scan.
+    ];
+
+  # Bootloader.
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Lisbon";
+
+  # Configure console keymap
+  console.keyMap = "pt-latin1";
+
+  # Enable zsh
+  programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.francisco = {
+    isNormalUser = true;
+    description = "Francisco";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+    ];
+  };
+
+  # Allow unfree packages
+  # nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     git
+  ];
+
+  # environment.interactiveShellInit = ''
+  # 	neofetch
+  # '';
+
+  
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ hosts/lightbulb/home.nix
@@ -0,0 +1,49 @@
+{ pkgs, profiles, configDir, ... }:
+let
+requiredPackages = with pkgs;
+[
+	bat
+	exiftool
+	fzf
+	neofetch
+	python3
+	rclone
+	ripgrep
+	unzip
+	zip
+];
+in
+{
+	home-manager = {
+		useGlobalPkgs = true;
+		useUserPackages = true;
+		extraSpecialArgs = {
+			inherit configDir;
+		};
+  };
+	
+	home-manager.users."francisco" = { pkgs, configDir,	...}: {
+		imports = with profiles.user;
+		[
+			alacritty
+			git
+			kitty
+			neovim
+			starship
+			tmux
+			yazi
+			zsh
+		];
+
+		home = {
+			username = "francisco";
+			homeDirectory = "/home/francisco";
+			stateVersion = "24.05";
+		};
+
+		# Let Home Manager install and manage itself.
+		programs.home-manager.enable = true;
+
+		home.packages = requiredPackages;   
+	};
+}
